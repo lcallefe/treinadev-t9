f6263466b757078771393ad18907c61cb50825e9
lcallefe <luciana_donadio@hotmail.com>
2022-09-28 17:36:33 -0300

    Usuário cadastra produto

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root "home#index"
   resources :warehouses, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :suppliers, only: [:index, :show, :new, :edit, :create, :update]
-  resources :product_models, only: [:index ]
+  resources :product_models, only: [:index , :new, :create, :show]
 end
